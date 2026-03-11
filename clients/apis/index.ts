512d166b922053659b80e91a369cb3bc09539f47
nonieKley <hei.oninaina@gmail.com>
2026-03-11 09:02:48 +0300

    chore: generate new client & docs

--- clients/apis/index.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./AuthApi";
-export * from "./ConfigurationApi";
-export * from "./GoalApi";
-export * from "./LabelApi";
-export * from "./ProjectApi";
-export * from "./TransactionApi";
-export * from "./WalletApi";
